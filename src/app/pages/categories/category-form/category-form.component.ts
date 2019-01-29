4ec16895a0805501cfd9c6f78e009fe3bcb79885
Gustavo Gialim <gustavo.gialim@gmail.com>
2019-01-28 22:57:25 -0200

    completing the category module

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -131,6 +131,9 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     if (error.status === 422) {
       this.serverErrorMessages = JSON.parse(error._body).errors;
     }
+    else {
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."];
+    }
   }
 
 }
